a6d49064dce230f9fdbfe31d32a32ad46a10e60a
antoniovenezia <antoniovenezia00@gmail.com>
2021-09-24 17:35:40 +0200

    commit

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    @foreach($articles as $article)
+        <div>
+            @dd($article)
+            <div>{{ $article->title }}</div>
+            <div>{{ $article->typeof }}</div>
+            <div>{{ $article->subtitle }}</div>
+            <div>{{ $article->paragraph }}</div>
+            <div>{{ $article->author }}</div>
+            <img src="{{ $article->picture }}" alt=""><br>
+            
+        </div>
+    @endforeach
+
+</body>
+</html>
